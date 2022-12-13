caefd02045cbbaae6c22347d475a3bb099205b11
liyingwen <1421379266@qq.com>
2022-12-13 10:39:14 +0800

    first commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true 
+  transpileDependencies: true ,
+  devServer:{
+    open:true,
+    host:"localhost"
+  }
 })
